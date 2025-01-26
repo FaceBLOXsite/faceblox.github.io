0973065f3510b68c7674f6eb1fe82c5ad6563ecd
FaceBLOXsite <jerejaatinen30@gmail.com>
2025-01-26 19:23:19 +0200

    Update faceblox.sql
    Signed-off-by: FaceBLOXsite <jerejaatinen30@gmail.com>

--- faceblox.sql
+++ faceblox.sql
@@ -1,8 +1,10 @@
-CREATE DATABASE faceblox;
+-- SQL schema for FaceBLOX users table
+
+CREATE DATABASE IF NOT EXISTS faceblox;
 
 USE faceblox;
 
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
